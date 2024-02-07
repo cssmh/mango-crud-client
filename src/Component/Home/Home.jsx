3d6e3fc98c58b8727bca9d30eb8e0de7db2b672c
momen210 <touristmomen@gmail.com>
2024-02-07 16:41:08 +0600

    form added

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,9 +1,39 @@
 const Home = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const handleAddMango = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const photo = form.photo.value;
+    const getMango = { name, photo };
+    console.log(getMango);
+  };
+
+  return (
+    <div className="text-center">
+      <h1 className="text-xl font-semibold my-5">Add Mango with Photo</h1>
+      <form onSubmit={handleAddMango}>
+        <input
+          type="text"
+          className="border-2 mb-1 rounded-md py-2 px-3 outline-blue-300"
+          placeholder="Mango type"
+          required
+          name="name"
+          id="1"
+        />
+        <br />
+        <input
+          type="text"
+          className="border-2 mb-1 rounded-md py-2 px-3 outline-blue-300"
+          placeholder="Mango photo URL"
+          required
+          name="photo"
+          id="2"
+        />
+        <br />
+        <input type="submit" className="btn mt-1" value="Add Mango" />
+      </form>
+    </div>
+  );
 };
 
 export default Home;
